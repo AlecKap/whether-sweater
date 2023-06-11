de42b2be54589d2b17d4bb7c1b0a73072e75ec75
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-10 18:26:27 -0600

    feat: Add get_lat_long method.

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,18 @@
+class MapquestService
+  def get_lat_long(location)
+    get_url("/geocoding/v1/address?location=#{location}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://www.mapquestapi.com') do |f|
+      f.params['key'] = ENV['MAPQUEST_API_KEY']
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
