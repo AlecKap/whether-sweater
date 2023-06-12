5cc79eaca1497492f93bf7335ac976b1184eecce
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-11 19:30:48 -0600

    route: Add users resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   namespace :api do
     namespace :v0 do
       resources :forecast, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
